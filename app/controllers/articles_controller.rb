dda4c2d62b7c1e265c15ff25443ebaefc217fab9
CharlieBotello <charlieabotello@gmail.com>
2018-06-28 16:29:54 -0500

    Articles edit controller/view connected, working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,11 @@ class ArticlesController < ApplicationController
 
   end
 
+  def edit
+    @article = Article.find(params[:id])
+    
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save 
@@ -13,6 +18,16 @@ class ArticlesController < ApplicationController
     else 
       render 'new'
     end 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'  
+    end
   end
 
   def show
